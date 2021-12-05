82dc25466014914116510f5e41303ca33842d8c2
yooookan <akiakira_ta_bashi_yookan@yahoo.co.jp>
2021-12-06 00:33:12 +0900

    add select form sample

--- app/app.py
+++ app/app.py
@@ -1,21 +1,18 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
 
-
-#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
-@app.route("/")
-def hello():
-    return "Hello World"
-
-
-#「/index」へアクセスがあった場合に、「index.html」を返す
-@app.route("/index")
-def index():
+@app.route("/", methods=['GET', 'POST'])
+def post():
+    if request.method == 'POST':
+        cat1 = request.form.get('cat1')
+        cat2 = request.form.get('cat2')
+        cat3 = request.form.get('cat3')
+        cat4 = request.form.get('cat4')
+        print('{}, {}, {}, {}'.format(cat1, cat2, cat3, cat4))
     return render_template("index.html")
-
 
 #おまじない
 if __name__ == "__main__":
